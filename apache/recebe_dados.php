2f1e42dc3f49947a17edb09d312657e5c593a634
Wesley Castro <54373294+wesleycstr@users.noreply.github.com>
2026-04-28 21:15:08 -0300

    Update and rename recebe_temperatura.php to recebe_dados.php

--- apache/recebe_dados.php
+++ apache/recebe_dados.php
@@ -3,7 +3,7 @@
 $host = "mariadb";
 $user = "root";
 $pass = "senha";
-$db   = "sensores";
+$db   = "siscav";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -13,9 +13,9 @@ if ($conn->connect_error) {
 
 /* recebe dados via GET ou POST */
 $sensor = $_REQUEST['sensor'] ?? null;
-$temp   = $_REQUEST['temperatura'] ?? null;
-$umid   = $_REQUEST['umidade'] ?? null;
-$gas   = $_REQUEST['gas'] ?? null;
+$temp   = $_REQUEST['temp'] ?? null;
+$umid   = $_REQUEST['umid'] ?? null;
+$gas_co   = $_REQUEST['gas'] ?? null;
 
 /* verifica se os dados existem */
 if ($sensor === null || $temp === null) {
@@ -23,8 +23,8 @@ if ($sensor === null || $temp === null) {
 }
 
 /* insere no banco */
-$sql = "INSERT INTO temperatura (sensor, valor, umidade, gas)
-        VALUES ('$sensor','$temp', '$umid', '$gas')";
+$sql = "INSERT INTO dados (id_sensor, temperatura, umidade, gas_co)
+        VALUES ('$sensor','$temp', '$umid', '$gas_co')";
 
 if ($conn->query($sql) === TRUE) {
     echo "OK";
