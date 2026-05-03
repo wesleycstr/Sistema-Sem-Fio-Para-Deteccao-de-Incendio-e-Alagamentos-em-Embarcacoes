4f7c5b6662a2b009f254ab40cdb71143ea7d31d9
Wesley Castro <54373294+wesleycstr@users.noreply.github.com>
2026-05-03 11:41:18 -0300

    Create verificar_alarme.php

---
+++ apache/verificar_alarme.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'conexao.php';
+
+$sql = "
+
+SELECT COUNT(*) AS total
+
+FROM alarme
+
+INNER JOIN sensores
+ON sensores.id = alarme.id_sensor
+
+WHERE
+alarme.status = 3
+AND sensores.alarme_sonoro = 1
+
+";
+
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+
+echo json_encode([
+    "alarme" => $row['total'] > 0
+]);
+
+?>
