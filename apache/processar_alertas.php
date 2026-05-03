bc9548b5c02ad5d2a5ac1560d72116794843af32
Wesley Castro <54373294+wesleycstr@users.noreply.github.com>
2026-05-03 15:46:00 -0300

    Update processar_alertas.php

--- apache/processar_alertas.php
+++ apache/processar_alertas.php
@@ -1,5 +1,4 @@
 <?php
-
 function processarAlertas($conn){
 
     $sql = "
@@ -15,7 +14,9 @@ function processarAlertas($conn){
 
         dados.temperatura,
         dados.umidade,
-        dados.gas_co
+        dados.gas_co,
+
+        alarme.status AS status_atual
 
     FROM sensores
 
@@ -30,35 +31,92 @@ function processarAlertas($conn){
 
     )
 
+    LEFT JOIN alarme
+    ON alarme.id_sensor = sensores.id
+
     ";
 
     $result = $conn->query($sql);
+
+    if(!$result){
+        return;
+    }
 
     while($row = $result->fetch_assoc()){
 
         $status = 1;
 
+        $idSensor = $row['id'];
+
+        /* TEMPERATURA */
+
         if(
+            isset($row['temperatura']) &&
             $row['temperatura'] > $row['temperatura_max']
         ){
             $status = 3;
         }
 
+        /* UMIDADE */
+
         if(
-            $row['umidade'] < $row['umidade_min']
-            ||
-            $row['umidade'] > $row['umidade_max']
+            isset($row['umidade']) &&
+            (
+                $row['umidade'] < $row['umidade_min']
+                ||
+                $row['umidade'] > $row['umidade_max']
+            )
         ){
             $status = 2;
         }
 
+        /* GÁS */
+
         if(
+            isset($row['gas_co']) &&
             $row['gas_co'] > $row['gas_max']
         ){
             $status = 3;
         }
 
-        $idSensor = $row['id'];
+        /* STATUS ANTERIOR */
+
+        $statusAnterior = isset($row['status_atual'])
+            ? $row['status_atual']
+            : -1;
+
+        /* REGISTRA SOMENTE SE MUDOU */
+
+        if($status != $statusAnterior){
+
+    switch($status){
+
+        case 1:
+            $evento =
+            "Sensor $idSensor voltou ao NORMAL";
+            break;
+
+        case 2:
+            $evento =
+            "Sensor $idSensor entrou em ATENÇÃO";
+            break;
+
+        case 3:
+            $evento =
+            "Sensor $idSensor entrou em ALARME";
+            break;
+
+        default:
+            $evento =
+            "Sensor $idSensor status desconhecido";
+
+    }
+
+            registrarLog($conn, $idSensor, $evento);
+
+        }
+
+        /* UPDATE */
 
         $sqlUpdate = "
 
