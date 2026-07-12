53cdfa3cedc8b8fa2b85836eb43f04777867c298
Wesley Castro <54373294+wesleycstr@users.noreply.github.com>
2026-07-12 15:07:27 -0300

    Update logs_tabela.php

--- apache/logs_tabela.php
+++ apache/logs_tabela.php
@@ -3,55 +3,38 @@
 include 'conexao.php';
 
 $sql = "
-
-SELECT *
-
+SELECT
+    logs.id,
+    logs.id_sensor,
+    sensores.device_token AS nome_sensor,
+    logs.data_hora,
+    logs.evento
 FROM logs
-
-ORDER BY data_hora DESC
-
+LEFT JOIN sensores ON sensores.id = logs.id_sensor
+ORDER BY logs.data_hora DESC
 LIMIT 100
-
 ";
 
 $result = $conn->query($sql);
 
-if($result->num_rows == 0){
-
+if ($result->num_rows == 0) {
     echo "
-
     <tr>
-
-        <td colspan='4'
-        style='text-align:center;'>
-
-        Nenhum log encontrado
-
+        <td colspan='4' style='text-align:center;'>
+            Nenhum log encontrado
         </td>
-
     </tr>
-
     ";
-
 }
 
-while($row = $result->fetch_assoc()){
-
-    ?>
-
+while ($row = $result->fetch_assoc()) {
+?>
     <tr>
-
         <td><?= $row['id'] ?></td>
-
-        <td><?= $row['id_sensor'] ?></td>
-
+        <td><?= $row['nome_sensor'] ?></td>
         <td><?= $row['data_hora'] ?></td>
-
         <td><?= $row['evento'] ?></td>
-
     </tr>
-
-    <?php
-
+<?php
 }
 ?>
